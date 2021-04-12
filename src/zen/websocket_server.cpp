977721240679279779f3898c51ffad062e37ff10
MarcoOl94 <marco.olivero94@libero.it>
2021-04-12 10:53:34 +0200

    Exposed websocket to external address

--- src/zen/websocket_server.cpp
+++ src/zen/websocket_server.cpp
@@ -1060,9 +1060,8 @@ bool StartWsServer()
 {
     try
     {
-        // ip address is not configurable as of now, it is locahost mandatorily
-        //std::string strAddress = GetArg("-wsaddress", "127.0.0.1");
-        std::string strAddress = "127.0.0.1";
+        // ip address is configurable now but just for testing purpose
+        std::string strAddress = GetArg("-wsaddress", "127.0.0.1");
         int port = GetArg("-wsport", 8888);
 
         ws_thread = boost::thread(ws_main, strAddress, port);
