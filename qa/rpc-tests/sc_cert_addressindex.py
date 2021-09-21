3b0d4ec0f94dad2af19681d89d6d0694787bb93c
Marco <marco.olivero94@libero.it>
2021-09-21 17:29:28 +0200

    Added getaddressmempool check inside sc_cert_addressindex.py test

--- qa/rpc-tests/sc_cert_addressindex.py
+++ qa/rpc-tests/sc_cert_addressindex.py
@@ -4,17 +4,15 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 from test_framework.test_framework import BitcoinTestFramework
-from test_framework.authproxy import JSONRPCException
-from test_framework.util import initialize_chain_clean, assert_equal, assert_true, assert_false, \
-    start_nodes, stop_nodes, get_epoch_data, \
-    sync_blocks, sync_mempools, connect_nodes_bi, wait_bitcoinds, mark_logs, \
-    swap_bytes
+from test_framework.util import initialize_chain_clean, assert_equal, \
+    start_nodes, get_epoch_data, \
+    sync_blocks, sync_mempools, connect_nodes_bi, mark_logs, \
+    swap_bytes, disconnect_nodes
 from test_framework.test_framework import MINIMAL_SC_HEIGHT, MINER_REWARD_POST_H200
 from test_framework.mc_test.mc_test import *
 import os
 from decimal import Decimal
 import time
-import pprint
 
 DEBUG_MODE = 1
 NUMB_OF_NODES = 2
@@ -27,6 +25,9 @@ MBTR_SC_FEE = Decimal('0')
 SC_COINS_MAT = 2
 MINIMAL_SC_HEIGHT = 420
 
+ORDINARY_OUTPUT = 0
+TOP_QUALITY_CERT_BACKWARD_TRANSFER = 1
+LOW_QUALITY_CERT_BACKWARD_TRANSFER = 2
 
 class sc_cert_addressindex(BitcoinTestFramework):
 
@@ -52,6 +53,22 @@ class sc_cert_addressindex(BitcoinTestFramework):
         sync_mempools(self.nodes[1:NUMB_OF_NODES])
         self.is_network_split = split
         self.sync_all()
+
+    def split_network(self):
+    # Split the network of three nodes into nodes 0 and 1.
+        assert not self.is_network_split
+        disconnect_nodes(self.nodes[0], 1)
+        disconnect_nodes(self.nodes[1], 0)
+        self.is_network_split = True
+
+    def join_network(self):
+        # Join the (previously split) network pieces together: 0-1
+        assert self.is_network_split
+        connect_nodes_bi(self.nodes, 0, 1)
+        connect_nodes_bi(self.nodes, 1, 0)
+        # self.sync_all()
+        time.sleep(2)
+        self.is_network_split = False    
 
     def run_test(self):
 
@@ -107,13 +124,18 @@ class sc_cert_addressindex(BitcoinTestFramework):
         
         ####### Test getaddressmempool ########
         addressmempool = self.nodes[1].getaddressmempool({"addresses":[tAddr1]})
-        #TODO: Verify that the immature BWT is returned
+        assert_equal(len(addressmempool),1)
+        assert_equal(addressmempool[0]['txid'], cert1)
+        assert_equal(addressmempool[0]['satoshis'], float(bwt_amount) * 1e8)
+        assert_equal(addressmempool[0]['address'], tAddr1)
+        assert_equal(addressmempool[0]['outstatus'], TOP_QUALITY_CERT_BACKWARD_TRANSFER)
+
         self.nodes[0].generate(1)
         self.sync_all()
 
         ####### Test getaddressmempool ########
         addressmempool = self.nodes[1].getaddressmempool({"addresses":[tAddr1]})
-        #assert_equal(addressmempool, [])
+        assert_equal(addressmempool, [])
         ####### Test getaddresstxids ########
         addresstxids = self.nodes[1].getaddresstxids({"addresses":[tAddr1]})
         #assert_equal(len(addresstxids),1)
@@ -130,38 +152,49 @@ class sc_cert_addressindex(BitcoinTestFramework):
         #Add to mempool Certificate 2 with quality = 7
         epoch_number, epoch_cum_tree_hash = get_epoch_data(scid, self.nodes[0], EPOCH_LENGTH)
         quality = 7  
-        bwt_amount = Decimal("7")      
+        bwt_amount2 = Decimal("7")      
         mark_logs("Add to mempool Certificate 2 with quality = {}...".format(quality), self.nodes, DEBUG_MODE)
         proof = mcTest.create_test_proof(
-            "sc1", scid_swapped, epoch_number, quality, MBTR_SC_FEE, FT_SC_FEE, epoch_cum_tree_hash, constant, [pkh_node1], [bwt_amount])
+            "sc1", scid_swapped, epoch_number, quality, MBTR_SC_FEE, FT_SC_FEE, epoch_cum_tree_hash, constant, [pkh_node1], [bwt_amount2])
 
-        amount_cert_1 = [{"pubkeyhash": pkh_node1, "amount": bwt_amount}]
+        amount_cert_2 = [{"pubkeyhash": pkh_node1, "amount": bwt_amount2}]
 
-        self.nodes[0].send_certificate(scid, epoch_number, quality,
-            epoch_cum_tree_hash, proof, amount_cert_1, FT_SC_FEE, MBTR_SC_FEE, CERT_FEE)
+        cert2 = self.nodes[0].send_certificate(scid, epoch_number, quality,
+            epoch_cum_tree_hash, proof, amount_cert_2, FT_SC_FEE, MBTR_SC_FEE, CERT_FEE)
         self.sync_all()
 
         ####### Test getaddressmempool ########
         addressmempool = self.nodes[1].getaddressmempool({"addresses":[tAddr1]})
-        #assert_equal(len(addressmempool), 1)
-        #TODO: Verify Certificate 2 is returned
+        assert_equal(len(addressmempool), 1)
+        assert_equal(addressmempool[0]['txid'], cert2)
+        assert_equal(addressmempool[0]['satoshis'], float(bwt_amount2) * 1e8)
+        assert_equal(addressmempool[0]['address'], tAddr1)
+        assert_equal(addressmempool[0]['outstatus'], TOP_QUALITY_CERT_BACKWARD_TRANSFER)
 
         quality = 9
-        bwt_amount = Decimal("9")
+        bwt_amount3 = Decimal("9")
         mark_logs("Add to mempool Certificate 3 with quality = {}...".format(quality), self.nodes, DEBUG_MODE)
         proof = mcTest.create_test_proof(
-            "sc1", scid_swapped, epoch_number, quality, MBTR_SC_FEE, FT_SC_FEE, epoch_cum_tree_hash, constant, [pkh_node1], [bwt_amount])
+            "sc1", scid_swapped, epoch_number, quality, MBTR_SC_FEE, FT_SC_FEE, epoch_cum_tree_hash, constant, [pkh_node1], [bwt_amount3])
 
-        amount_cert_1 = [{"pubkeyhash": pkh_node1, "amount": bwt_amount}]
+        amount_cert_3 = [{"pubkeyhash": pkh_node1, "amount": bwt_amount3}]
 
-        self.nodes[0].send_certificate(scid, epoch_number, quality,
-            epoch_cum_tree_hash, proof, amount_cert_1, FT_SC_FEE, MBTR_SC_FEE, CERT_FEE)
+        cert3 = self.nodes[0].send_certificate(scid, epoch_number, quality,
+            epoch_cum_tree_hash, proof, amount_cert_3, FT_SC_FEE, MBTR_SC_FEE, CERT_FEE)
         self.sync_all()
 
         ####### Test getaddressmempool ########
         addressmempool = self.nodes[1].getaddressmempool({"addresses":[tAddr1]})
-        #assert_equal(len(addressmempool), 2)
-        #TODO: Verify Certificate 2 and Cerificate 3 are returned   
+        assert_equal(len(addressmempool), 2)
+        assert_equal(addressmempool[0]['txid'], cert2)
+        assert_equal(addressmempool[0]['satoshis'], float(bwt_amount2) * 1e8)
+        assert_equal(addressmempool[0]['address'], tAddr1)
+        assert_equal(addressmempool[0]['outstatus'], LOW_QUALITY_CERT_BACKWARD_TRANSFER)
+
+        assert_equal(addressmempool[1]['txid'], cert3)
+        assert_equal(addressmempool[1]['satoshis'], float(bwt_amount3) * 1e8)
+        assert_equal(addressmempool[1]['address'], tAddr1)
+        assert_equal(addressmempool[1]['outstatus'], TOP_QUALITY_CERT_BACKWARD_TRANSFER)
 
         self.nodes[0].generate(1)
         self.sync_all()
@@ -172,7 +205,7 @@ class sc_cert_addressindex(BitcoinTestFramework):
         #TODO: Verify that Certificate1,2,3 are in the response     
         ####### Test getaddressmempool ########
         addressmempool = self.nodes[1].getaddressmempool({"addresses":[tAddr1]})
-        #assert_equal(len(addressmempool), 0)
+        assert_equal(len(addressmempool), 0)
         ####### Test getaddressbalance ########
         addressbalance = self.nodes[1].getaddressbalance({"addresses":[tAddr1]})
         addressbalanceWithImmature = self.nodes[1].getaddressbalance({"addresses":[tAddr1]}, True)  
@@ -181,34 +214,50 @@ class sc_cert_addressindex(BitcoinTestFramework):
         addressutxoWithImmature = self.nodes[1].getaddressutxos({"addresses":[tAddr1]}, True)
         #TODO: Verify the difference between addressutxo and addressutxoWithImmature    
 
+
+        # Split the network: (0)--(1) / (2)
+        mark_logs("\nSplit network", self.nodes, DEBUG_MODE)
+        self.split_network()
+        mark_logs("The network is split: 0-1 .. 2", self.nodes, DEBUG_MODE)
+
+
         #Mine a block with Certificate 4 with quality = 11 and Certificate 5 with quality = 13
         epoch_number, epoch_cum_tree_hash = get_epoch_data(scid, self.nodes[0], EPOCH_LENGTH)
         quality = 11
-        bwt_amount = Decimal("11")
+        bwt_amount4 = Decimal("11")
         mark_logs("Create a Certificate 4 with quality = {}...".format(quality), self.nodes, DEBUG_MODE)
         proof = mcTest.create_test_proof(
-            "sc1", scid_swapped, epoch_number, quality, MBTR_SC_FEE, FT_SC_FEE, epoch_cum_tree_hash, constant, [pkh_node1], [bwt_amount])
+            "sc1", scid_swapped, epoch_number, quality, MBTR_SC_FEE, FT_SC_FEE, epoch_cum_tree_hash, constant, [pkh_node1], [bwt_amount4])
 
-        amount_cert_1 = [{"pubkeyhash": pkh_node1, "amount": bwt_amount}]
+        amount_cert_4 = [{"pubkeyhash": pkh_node1, "amount": bwt_amount4}]
 
         self.nodes[0].send_certificate(scid, epoch_number, quality,
-            epoch_cum_tree_hash, proof, amount_cert_1, FT_SC_FEE, MBTR_SC_FEE, CERT_FEE)
-        self.sync_all()
+            epoch_cum_tree_hash, proof, amount_cert_4, FT_SC_FEE, MBTR_SC_FEE, CERT_FEE)
 
         quality = 13
-        bwt_amount = Decimal("13")
+        bwt_amount5 = Decimal("13")
         mark_logs("Create a Certificat 5 with quality = {}...".format(quality), self.nodes, DEBUG_MODE)
         proof = mcTest.create_test_proof(
-            "sc1", scid_swapped, epoch_number, quality, MBTR_SC_FEE, FT_SC_FEE, epoch_cum_tree_hash, constant, [pkh_node1], [bwt_amount])
+            "sc1", scid_swapped, epoch_number, quality, MBTR_SC_FEE, FT_SC_FEE, epoch_cum_tree_hash, constant, [pkh_node1], [bwt_amount5])
 
-        amount_cert_1 = [{"pubkeyhash": pkh_node1, "amount": bwt_amount}]
+        amount_cert_5 = [{"pubkeyhash": pkh_node1, "amount": bwt_amount5}]
 
         self.nodes[0].send_certificate(scid, epoch_number, quality,
-            epoch_cum_tree_hash, proof, amount_cert_1, FT_SC_FEE, MBTR_SC_FEE, CERT_FEE)
-        self.sync_all()
+            epoch_cum_tree_hash, proof, amount_cert_5, FT_SC_FEE, MBTR_SC_FEE, CERT_FEE)
 
         self.nodes[0].generate(1)
-        self.sync_all()
+
+        # Checking the network chain tips
+        mark_logs("\nChecking network chain tips...", self.nodes, DEBUG_MODE)
+        print(self.nodes[0].getblockchaininfo()['blocks'])
+        print(self.nodes[1].getblockchaininfo()['blocks'])
+
+        assert_equal(self.nodes[0].getblockchaininfo()['blocks'], 524)
+        assert_equal(self.nodes[1].getblockchaininfo()['blocks'], 523)
+
+        mark_logs("\nJoining network", self.nodes, DEBUG_MODE)
+        self.join_network()
+        mark_logs("\nNetwork joined", self.nodes, DEBUG_MODE)
 
         ####### Test getaddresstxids ########
         addresstxids = self.nodes[1].getaddresstxids({"addresses":[tAddr1]})
@@ -216,7 +265,7 @@ class sc_cert_addressindex(BitcoinTestFramework):
         #TODO: Verify that Certificate1,2,3,4,5 are in the response     
         ####### Test getaddressmempool ########
         addressmempool = self.nodes[1].getaddressmempool({"addresses":[tAddr1]})
-        #assert_equal(len(addressmempool), 0)
+        assert_equal(len(addressmempool), 0)
         ####### Test getaddressbalance ########
         addressbalance = self.nodes[1].getaddressbalance({"addresses":[tAddr1]})
         addressbalanceWithImmature = self.nodes[1].getaddressbalance({"addresses":[tAddr1]}, True)  
@@ -224,6 +273,12 @@ class sc_cert_addressindex(BitcoinTestFramework):
         addressutxo = self.nodes[1].getaddressutxos({"addresses":[tAddr1]})
         addressutxoWithImmature = self.nodes[1].getaddressutxos({"addresses":[tAddr1]}, True)
         #TODO: Verify the difference between addressutxo and addressutxoWithImmature    
+
+        # Checking the network chain tips
+        mark_logs("\nChecking network chain tips...", self.nodes, DEBUG_MODE)
+        for i in range(0, NUMB_OF_NODES):
+            print(self.nodes[i].getblockchaininfo()['blocks'])
+            assert_equal(self.nodes[i].getblockchaininfo()['blocks'],524)
 
 if __name__ == '__main__':
     sc_cert_addressindex().main()
--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -302,7 +302,7 @@ static const CRPCCommand vRPCCommands[] =
     { "blockchain",         "getbestblockhash",       &getbestblockhash,       true  },
     { "blockchain",         "getblockcount",          &getblockcount,          true  },
     { "blockchain",         "getblock",               &getblock,               true  },
-    { "blockchain",         "getblockdeltas",         &getblockdeltas,         false },
+    //{ "blockchain",         "getblockdeltas",         &getblockdeltas,         false },
     { "blockchain",         "getblockhashes",         &getblockhashes,         true  },
     { "blockchain",         "getblockhash",           &getblockhash,           true  },
     { "blockchain",         "getblockfinalityindex",  &getblockfinalityindex,  true  },
